82afb0da9c56ea3895a6e97f037ced6c5d004a04
eggcognito1337 <eggcognito1337@gmail.com>
2019-09-25 14:36:25 +0200

    2

---
+++ app/src/main/java/com/TcLt/activithing/Main2Activity.java
@@ -0,0 +1,33 @@
+package com.TcLt.activithing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity
+{
+
+    Button buttonVissza;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        buttonVissza = findViewById(R.id.buttonVissza);
+        buttonVissza.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent vissza = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(vissza);
+                finish();
+            }
+        });
+    }
+}
